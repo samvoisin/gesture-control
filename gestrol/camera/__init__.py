4644d9c7bbbe1a1a5ce9b85653c646611ea52dd4
Sam Voisin <samvoisin@protonmail.com>
2022-07-14 11:00:02 -0400

    add video loader interface

--- gestrol/camera/__init__.py
+++ gestrol/camera/__init__.py
@@ -1,2 +1,3 @@
 # gestrol library
 from gestrol.camera.open_cv import OpenCVCameraInterface  # noqa
+from gestrol.camera.video_loader import VideoLoaderInterface  # noqa
---
+++ gestrol/camera/video_loader.py
@@ -0,0 +1,33 @@
+# standard libraries
+import logging
+import sys
+from pathlib import Path
+
+# external libraries
+import cv2
+import numpy as np
+
+# gestrol library
+from gestrol.camera.base import CameraInterface
+
+logging.basicConfig(
+    stream=sys.stdout,
+    level=logging.INFO,
+    format="[%(asctime)s] (%(filename)s:%(lineno)d) %(levelname)s : %(message)s",
+)
+
+logger = logging.getLogger()
+
+
+class VideoLoaderInterface(CameraInterface):
+    def __init__(self, video_path: Path):
+        self.camera = cv2.VideoCapture(str(video_path))
+        logger.info("Video loaded")
+
+    def __del__(self):
+        self.camera.release()
+        logger.info("Video complete")
+
+    def get_frame(self) -> np.ndarray:
+        _, frame = self.camera.read()
+        return frame
