8eae5d6a1b04ec52fb01c7938ed41af916c09369
Sam Voisin <samvoisin@proton.me>
2022-10-11 16:09:40 -0400

    cms and simple test

---
+++ gestrol/control_mode_switch.py
@@ -0,0 +1,38 @@
+# standard libraries
+from typing import Optional
+
+
+class ControlModeSwitch:
+    """
+    Switch to toggle gesture classifier with control interface.
+
+    TODO: Does this live on it's own? or is does it belong to control interface?
+    """
+
+    def __init__(self, control_mode_signal: int):
+        self.control_mode_signal = control_mode_signal
+        self.control_mode = False
+
+    def toggle_control_mode(self):
+        """
+        Toggle control mode on or off.
+        """
+        self.control_mode = not self.control_mode
+
+    def assess_gesture_signal(self, gesture_signal: int) -> Optional[int]:
+        """
+        Assess incoming signal from gesture classifier to determine if it should be passed on to control interface.
+
+        Args:
+            gesture_signal (int): Signal from gesture classifier
+
+        Returns:
+            Optional[int]: Signal passed on to control interface. May be `None`.
+        """
+        if gesture_signal == self.control_mode_signal:  # check for control mode signal
+            self.toggle_control_mode()
+            return None
+        elif self.control_mode:  # not control mode signal; check for control mode
+            return gesture_signal
+        else:  # not control mode signal and control mode is off
+            return None
