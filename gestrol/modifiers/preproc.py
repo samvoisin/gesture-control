c3081f9fa6a1b822073604de16aa3afd9a245548
Sam Voisin <samvoisin@protonmail.com>
2023-01-24 06:43:01 -0500

    preproc modifier update

--- gestrol/modifiers/preproc.py
+++ gestrol/modifiers/preproc.py
@@ -1,5 +1,4 @@
 # external libraries
-from torch import Tensor
 from torchvision import transforms
 
 # gestrol library
@@ -7,20 +6,15 @@ from gestrol.modifiers.base import Frame, FrameModifier
 
 
 class FasterRCNNPreprocModifier(FrameModifier):
-    """
-    preprocess a frame before feeding into Faster R-CNN hand identifier.
-
-    NOTE: this can possibly be deprecated. the pytorch of FRCNN normalizes images on input.
-    """
+    """Preprocess a frame before feeding into Faster R-CNN hand identifier."""
 
     def __init__(self):
         super().__init__()
         self.fcn_composition = transforms.Compose(
             [
-                transforms.ToTensor(),
                 transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
             ]
         )
 
-    def __call__(self, frame: Frame) -> Tensor:
+    def __call__(self, frame: Frame) -> Frame:
         return self.fcn_composition(frame)
