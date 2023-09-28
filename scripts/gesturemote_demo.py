0de8ac189c826a7d3d46e26373738603db5e0041
Sam Voisin <samvoisin@protonmail.com>
2023-09-28 12:29:50 -0400

    scripts intermed

--- scripts/gesturemote_demo.py
+++ scripts/gesturemote_demo.py
@@ -1,7 +1,6 @@
-# gestrol library
+# gesturemote library
 from gesturemote.gesture_controller import GestureController
-from gesturemote.macos_routines import MACOS_ROUTINES
 
 if __name__ == "__main__":
-    gc = GestureController(routines=MACOS_ROUTINES, monitor_fps=True, verbose=True)
-    gc.activate(video_preview=True)  # costs about 0.5 FPS
+    gc = GestureController(monitor_fps=True)
+    gc.activate(video_preview=True)
