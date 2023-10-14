35c4dd0bd358e5c47aba6fcf4e5feaf6ef417725
Sam Voisin <samvoisin@protonmail.com>
2023-10-14 10:28:19 -0400

    remove old demo script

--- scripts/gesturemote_demo.py
+++
@@ -1,6 +0,0 @@
-# gesturemote library
-from gesturemote.gesture_controller import GestureController
-
-if __name__ == "__main__":
-    gc = GestureController(monitor_fps=True, verbose=True)
-    gc.activate(video_preview=False)
