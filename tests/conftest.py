aaf27c3cdb2d230c4f877be0cfe519d595715610
Sam Voisin <samvoisin@protonmail.com>
2024-04-24 14:40:04 -0400

    set env var at workflow level

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,29 +1,8 @@
-import os
-
 import numpy as np
 import pytest
 from torch import Tensor
 
 from gesturemote.camera.base import CameraInterface
-
-
-@pytest.fixture(scope="function", autouse=True)
-def set_display_env():
-    # Check if the DISPLAY environment variable is already set
-    old_display = os.environ.get("DISPLAY")
-
-    # Set the DISPLAY environment variable to a dummy value if not present
-    if old_display is None:
-        os.environ["DISPLAY"] = ":99"
-
-    # Yield to run the test
-    yield
-
-    # Cleanup: Restore the original DISPLAY environment variable
-    if old_display is None:
-        del os.environ["DISPLAY"]
-    else:
-        os.environ["DISPLAY"] = old_display
 
 
 @pytest.fixture
